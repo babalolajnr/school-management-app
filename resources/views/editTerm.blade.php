d468c4a70ffeacab15a22dff905935e2fb9968c1
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-06-21 14:52:58 +0100

    add script slot

--- resources/views/editTerm.blade.php
+++ resources/views/editTerm.blade.php
@@ -3,7 +3,7 @@
     </x-slot>
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
-        
+
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
@@ -58,5 +58,5 @@
         </section>
         <!-- /.content -->
     </div>
-    
+    <x-slot name="scripts"></x-slot>
 </x-app-layout>
