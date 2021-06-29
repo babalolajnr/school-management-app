f228a71dcba0de1c19c0ea6dda5e2dd2b8c0dcaf
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-06-29 10:37:01 +0100

    create user teacher settings controller

---
+++ app/Http/Controllers/UserTeacherSettingsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserTeacherSettings;
+use Illuminate\Http\Request;
+
+class UserTeacherSettingsController extends Controller
+{
+    public function toggleDarkMode(Request $request)
+    {
+        if ($request->darkmode == 'true') {
+            $darkmode = true;
+        } else {
+            $darkmode = false;
+        }
+
+        if (auth('web')->check()) {
+            UserTeacherSettings::updateOrCreate(
+                ['user_id' => auth('web')->user()->id],
+                [
+                    'dark_mode' => $darkmode,
+                    'teacher_id' => null
+                ]
+            );
+
+            return response()->json(['status' => 'success', 'darkmode' => $darkmode]);
+        };
+    }
+}
