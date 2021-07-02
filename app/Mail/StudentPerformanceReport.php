5d2afcd5b14c6c02dcfd9064ef24a2804469e9d1
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-07-02 17:44:06 +0100

    create student performance report mail

---
+++ app/Mail/StudentPerformanceReport.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Period;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\URL;
+
+class StudentPerformanceReport extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $student;
+    private $periodSlug;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($student, $periodSlug)
+    {
+        $this->student = $student;
+        $this->periodSlug = $periodSlug;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $url = URL::temporarySignedRoute('result.guardian.show.performance', now()->addDays(7), ['student' => $this->student, 'periodSlug' => $this->periodSlug]);
+        $period = Period::where('slug', $this->periodSlug)->first();
+        $term = $period->term;
+        $academicSession = $period->academicSession;
+        $content = [
+            'url' => $url,
+            'student' => $this->student,
+            'guardian' => $this->student->guardian,
+            'term' => $term,
+            'academicSession' => $academicSession
+        ];
+
+        return $this->markdown('emails.performanceReport')
+            ->subject('Performance Report')->with('content', $content);
+    }
+}
