3352af2f5fa4aafe3f7e086e16c862d62847198d
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-26 08:15:49 +0100

    add online status indicator

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,6 +14,15 @@
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                @if (Cache::has('user-is-online-' . auth()->user()->id))
+                    <i class="fas fa-circle text-green-700 text-sm" title="Online"></i>
+                @else
+                    <i class="fas fa-circle text-red-600 text-sm" title="Offline"></i>
+                @endif
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
         </li>
