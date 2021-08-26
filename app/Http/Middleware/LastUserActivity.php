cfae94b96f4393dcd62bc2aae051df1fcb4b56c6
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-26 13:52:00 +0100

    add condition for when auth guard is 'teacher'

--- app/Http/Middleware/LastUserActivity.php
+++ app/Http/Middleware/LastUserActivity.php
@@ -22,9 +22,14 @@ class LastUserActivity
     public function handle(Request $request, Closure $next)
     {
         if (auth()->check()) {
-           
+
             $expiresAt = Carbon::now()->addMinutes(2);
-            Cache::put('user-is-online-' . $request->user()->id, true, $expiresAt);
+
+            if (auth('web')->check()) {
+                Cache::put('user-is-online-' . $request->user()->id, true, $expiresAt);
+            } else {
+                Cache::put('teacher-is-online-' . $request->user()->id, true, $expiresAt);
+            }
 
             $request->user()->update(['last_seen' => Carbon::now()->format('Y-m-d H:i:s')]);
         }
