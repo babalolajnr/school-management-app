4de1aa23d5dec2a84b894cfd364a5a4020ca459b
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-28 15:43:01 +0100

    move custom js code to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -85,7 +85,7 @@
     if (el.tagName.toLowerCase() !== 'template') {
       console.warn(`Alpine: [${directive}] directive should only be added to <template> tags. See https://github.com/alpinejs/alpine#${directive}`);
     } else if (el.content.childElementCount !== 1) {
-      console.warn(`Alpine: <template> tag with [${directive}] encountered with multiple element roots. Make sure <template> only has a single child element.`);
+      console.warn(`Alpine: <template> tag with [${directive}] encountered with an unexpected number of root elements. Make sure <template> has a single root element. `);
     }
   }
   function kebabCase(subject) {
@@ -123,6 +123,10 @@
     console.warn(`Alpine Error: "${error}"\n\nExpression: "${expression}"\nElement:`, el);
 
     if (!isTesting()) {
+      Object.assign(error, {
+        el,
+        expression
+      });
       throw error;
     }
   };
@@ -582,7 +586,7 @@
     let forIteratorRE = /,([^,\}\]]*)(?:,([^,\}\]]*))?$/;
     let stripParensRE = /^\(|\)$/g;
     let forAliasRE = /([\s\S]*?)\s+(?:in|of)\s+([\s\S]*)/;
-    let inMatch = expression.match(forAliasRE);
+    let inMatch = String(expression).match(forAliasRE);
     if (!inMatch) return;
     let res = {};
     res.items = inMatch[2].trim();
@@ -628,7 +632,7 @@
 
     let items = component.evaluateReturnExpression(el, iteratorNames.items, extraVars); // This adds support for the `i in n` syntax.
 
-    if (isNumeric(items) && items > 0) {
+    if (isNumeric(items) && items >= 0) {
       items = Array.from(Array(items).keys(), i => i + 1);
     }
 
@@ -679,7 +683,7 @@
     if (attrName === 'value') {
       if (Alpine.ignoreFocusedForValueBinding && document.activeElement.isSameNode(el)) return; // If nested model key is undefined, set the default value to empty string.
 
-      if (value === undefined && expression.match(/\./)) {
+      if (value === undefined && String(expression).match(/\./)) {
         value = '';
       }
 
@@ -762,7 +766,7 @@
 
   function handleTextDirective(el, output, expression) {
     // If nested model key is undefined, set the default value to empty string.
-    if (output === undefined && expression.match(/\./)) {
+    if (output === undefined && String(expression).match(/\./)) {
       output = '';
     }
 
@@ -867,8 +871,12 @@
       event = camelCase(event);
     }
 
+    let handler, listenerTarget;
+
     if (modifiers.includes('away')) {
-      let handler = e => {
+      listenerTarget = document;
+
+      handler = e => {
         // Don't do anything if the click came from the element or within it.
         if (el.contains(e.target)) return; // Don't do anything if this element isn't currently visible.
 
@@ -880,14 +888,11 @@
         if (modifiers.includes('once')) {
           document.removeEventListener(event, handler, options);
         }
-      }; // Listen for this event at the root level.
-
-
-      document.addEventListener(event, handler, options);
+      };
     } else {
-      let listenerTarget = modifiers.includes('window') ? window : modifiers.includes('document') ? document : el;
+      listenerTarget = modifiers.includes('window') ? window : modifiers.includes('document') ? document : el;
 
-      let handler = e => {
+      handler = e => {
         // Remove this global event handler if the element that declared it
         // has been removed. It's now stale.
         if (listenerTarget === window || listenerTarget === document) {
@@ -921,15 +926,15 @@
           });
         }
       };
-
-      if (modifiers.includes('debounce')) {
-        let nextModifier = modifiers[modifiers.indexOf('debounce') + 1] || 'invalid-wait';
-        let wait = isNumeric(nextModifier.split('ms')[0]) ? Number(nextModifier.split('ms')[0]) : 250;
-        handler = debounce(handler, wait);
-      }
-
-      listenerTarget.addEventListener(event, handler, options);
     }
+
+    if (modifiers.includes('debounce')) {
+      let nextModifier = modifiers[modifiers.indexOf('debounce') + 1] || 'invalid-wait';
+      let wait = isNumeric(nextModifier.split('ms')[0]) ? Number(nextModifier.split('ms')[0]) : 250;
+      handler = debounce(handler, wait);
+    }
+
+    listenerTarget.addEventListener(event, handler, options);
   }
 
   function runListenerHandler(component, expression, e, extraVars) {
@@ -1513,9 +1518,11 @@
         initReturnedCallback = this.evaluateReturnExpression(this.$el, initExpression);
         this.pauseReactivity = false;
       } // Register all our listeners and set all our attribute bindings.
+      // If we're cloning a component, the third parameter ensures no duplicate
+      // event listeners are registered (the mutation observer will take care of them)
 
 
-      this.initializeElements(this.$el); // Use mutation observer to detect new elements being added within this component at run-time.
+      this.initializeElements(this.$el, () => {}, componentForClone); // Use mutation observer to detect new elements being added within this component at run-time.
       // Alpine's just so darn flexible amirite?
 
       this.listenForNewElementsToInitialize();
@@ -1604,28 +1611,28 @@
       });
     }
 
-    initializeElements(rootEl, extraVars = () => {}) {
+    initializeElements(rootEl, extraVars = () => {}, componentForClone = false) {
       this.walkAndSkipNestedComponents(rootEl, el => {
         // Don't touch spawns from for loop
         if (el.__x_for_key !== undefined) return false; // Don't touch spawns from if directives
 
         if (el.__x_inserted_me !== undefined) return false;
-        this.initializeElement(el, extraVars);
+        this.initializeElement(el, extraVars, componentForClone ? false : true);
       }, el => {
-        el.__x = new Component(el);
+        if (!componentForClone) el.__x = new Component(el);
       });
       this.executeAndClearRemainingShowDirectiveStack();
       this.executeAndClearNextTickStack(rootEl);
     }
 
-    initializeElement(el, extraVars) {
+    initializeElement(el, extraVars, shouldRegisterListeners = true) {
       // To support class attribute merging, we have to know what the element's
       // original class attribute looked like for reference.
       if (el.hasAttribute('class') && getXAttrs(el, this).length > 0) {
         el.__x_original_classes = convertClassStringToArray(el.getAttribute('class'));
       }
 
-      this.registerListeners(el, extraVars);
+      shouldRegisterListeners && this.registerListeners(el, extraVars);
       this.resolveBoundAttributes(el, true, extraVars);
     }
 
@@ -1842,7 +1849,7 @@
   }
 
   const Alpine = {
-    version: "2.8.0",
+    version: "2.8.2",
     pauseMutationObserver: false,
     magicProperties: {},
     onComponentInitializeds: [],
@@ -3794,6 +3801,68 @@ module.exports = {
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 __webpack_require__(/*! alpinejs */ "./node_modules/alpinejs/dist/alpine.js");
+
+var date = new Date().getFullYear();
+document.getElementById('footerDate').innerHTML = date;
+var sunIcon = "<i class='fas fa-sun text-yellow-400'></i>";
+var moonIcon = "<i class='fas fa-moon'></i>";
+var Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 5000
+});
+$(function () {
+  toastrAlert();
+  darkMode();
+});
+
+function toastrAlert() {
+  var Success = document.getElementById('success');
+  var Error = document.getElementById('error'); // if data-success = 'true' display alert
+
+  if (Success.dataset.success == 'true') Toast.fire({
+    icon: 'success',
+    title: JSON.parse(Success.dataset.successMessage)
+  });
+  if (Error.dataset.error == 'true') Toast.fire({
+    icon: 'error',
+    title: JSON.parse(Error.dataset.errorMessage)
+  });
+}
+
+function darkMode() {
+  //get darkmode status from localStorage
+  var darkmodeStatus = localStorage.getItem('dark-mode');
+
+  if (darkmodeStatus == "true") {
+    $('#navbar').removeClass('navbar-white navbar-light');
+    $("#sidebar").addClass('sidebar-dark-navy');
+    $('body').addClass('dark-mode');
+    $('#navbar').addClass('navbar-dark');
+    $("#dark-mode").append(sunIcon);
+  } else {
+    $("#sidebar").addClass('sidebar-light-navy');
+    $("#dark-mode").append(moonIcon);
+  }
+}
+
+$("#dark-mode").click(function () {
+  var darkmodeStatus = localStorage.getItem('dark-mode');
+  $('body').toggleClass('dark-mode');
+  $("#sidebar").toggleClass('sidebar-light-navy sidebar-dark-navy');
+  $('#navbar').toggleClass('navbar-dark navbar-white navbar-light');
+
+  if (darkmodeStatus == "true") {
+    $("#dark-mode").children().remove();
+    $("#dark-mode").append(moonIcon);
+    localStorage.setItem('dark-mode', false);
+  } else {
+    $("#dark-mode").children().remove();
+    $("#dark-mode").append(sunIcon);
+    localStorage.setItem('dark-mode', true);
+  }
+});
 
 /***/ }),
 
@@ -21304,7 +21373,8 @@ process.umask = function() { return 0; };
 /******/ 				}
 /******/ 				if(fulfilled) {
 /******/ 					deferred.splice(i--, 1)
-/******/ 					result = fn();
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
 /******/ 				}
 /******/ 			}
 /******/ 			return result;
@@ -21378,12 +21448,14 @@ process.umask = function() { return 0; };
 /******/ 			// add "moreModules" to the modules object,
 /******/ 			// then flag all "chunkIds" as loaded and fire callback
 /******/ 			var moduleId, chunkId, i = 0;
-/******/ 			for(moduleId in moreModules) {
-/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
-/******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
 /******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
 /******/ 			}
-/******/ 			if(runtime) var result = runtime(__webpack_require__);
 /******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
 /******/ 			for(;i < chunkIds.length; i++) {
 /******/ 				chunkId = chunkIds[i];
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,75 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+const date = new Date().getFullYear();
+document.getElementById('footerDate').innerHTML = date;
+
+const sunIcon = "<i class='fas fa-sun text-yellow-400'></i>"
+const moonIcon = "<i class='fas fa-moon'></i>"
+
+var Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 5000
+});
+
+
+$(function () {
+    toastrAlert()
+    darkMode()
+});
+
+function toastrAlert() {
+    let Success = document.getElementById('success')
+    let Error = document.getElementById('error')
+
+    // if data-success = 'true' display alert
+    if (Success.dataset.success == 'true')
+        Toast.fire({
+            icon: 'success',
+            title: JSON.parse(Success.dataset.successMessage)
+        })
+
+    if (Error.dataset.error == 'true')
+        Toast.fire({
+            icon: 'error',
+            title: JSON.parse(Error.dataset.errorMessage)
+        })
+}
+
+function darkMode() {
+    //get darkmode status from localStorage
+    const darkmodeStatus = localStorage.getItem('dark-mode')
+
+    if (darkmodeStatus == "true") {
+        $('#navbar').removeClass('navbar-white navbar-light')
+        $("#sidebar").addClass('sidebar-dark-navy')
+        $('body').addClass('dark-mode')
+        $('#navbar').addClass('navbar-dark')
+        $("#dark-mode").append(sunIcon)
+    } else {
+        $("#sidebar").addClass('sidebar-light-navy')
+        $("#dark-mode").append(moonIcon)
+    }
+}
+
+$("#dark-mode").click(function () {
+    const darkmodeStatus = localStorage.getItem('dark-mode')
+
+    $('body').toggleClass('dark-mode')
+    $("#sidebar").toggleClass('sidebar-light-navy sidebar-dark-navy')
+    $('#navbar').toggleClass('navbar-dark navbar-white navbar-light')
+
+
+    if (darkmodeStatus == "true") {
+        $("#dark-mode").children().remove()
+        $("#dark-mode").append(moonIcon)
+        localStorage.setItem('dark-mode', false)
+    } else {
+        $("#dark-mode").children().remove()
+        $("#dark-mode").append(sunIcon)
+        localStorage.setItem('dark-mode', true)
+    }
+})
