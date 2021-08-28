7b3d0107b0a4f98062882f06fa2eb0e54387688f
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-08-28 16:36:54 +0100

    change sidebar user icon

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -12,7 +12,7 @@
            <!-- Sidebar user (optional) -->
            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                <div class="image">
-                   <img src="{{ asset('images/user1.svg') }}" class="img-circle elevation-2" alt="User Image">
+                   <img src="https://img.icons8.com/plumpy/192/000000/user.png" class="img-circle elevation-2" alt="User Image">
                </div>
                <div class="info">
                    @if (Auth::guard('web')->user())
