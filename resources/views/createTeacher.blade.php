afd19596a69f4bf45df3759b660c5bbe3477c78e
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-17 12:53:40 +0100

    reduce card width

--- resources/views/createTeacher.blade.php
+++ resources/views/createTeacher.blade.php
@@ -25,7 +25,7 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-6">
                         <!-- Default box -->
                         <div class="card">
                             <div class="card-header">
