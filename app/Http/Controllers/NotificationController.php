f6bd6072b1cd1e8ad6e59e3756c817fc36938ce6
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-10-28 16:44:39 +0100

    add store method

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\NotificationRequest;
+use App\Models\Teacher;
+use App\Models\User;
+use App\Notifications\AppNotification;
 
 class NotificationController extends Controller
 {
+    /**
+     * Index
+     *
+     * @return  \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function index()
     {
         return view('notifications');
+    }
+
+    public function store(NotificationRequest $request)
+    {
+        $data = $request->validated();
+
+        if ($data['notification-type'] == 'App Notification') {
+            $recipients = $data['to'];
+            $notification = ["title" => $data['title'], "message" => $data['message']];
+
+            if ($recipients == 'Admins') {
+                User::all()->map(function ($user) use ($notification) {
+                    $user->notify(new AppNotification($notification));
+                });
+            } elseif ($recipients == 'Master Users') {
+                User::where('user_type', 'master')->get()->map(function ($user) use ($notification) {
+                    $user->notify(new AppNotification($notification));
+                });
+            } elseif ($recipients == 'Teachers') {
+                Teacher::all()->map(function ($teacher) use ($notification) {
+                    $teacher->notify(new AppNotification($notification));
+                });
+            } elseif ($recipients == 'All') {
+                
+                Teacher::all()->map(function ($teacher) use ($notification) {
+                    $teacher->notify(new AppNotification($notification));
+                });
+                User::all()->map(function ($user) use ($notification) {
+                    $user->notify(new AppNotification($notification));
+                });
+            }
+
+            return back()->with('success', 'Notification sent');
+        }
     }
 }
