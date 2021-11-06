0eea40efd3e3164a7bbfee30c7937ca8b76df439
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-11-06 10:16:26 +0100

    add classrooms relationship

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Represents an arm of a classroom
+ */
 class Branch extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function classrooms()
+    {
+        $this->belongsToMany(Classroom::class);
+    }
 }
