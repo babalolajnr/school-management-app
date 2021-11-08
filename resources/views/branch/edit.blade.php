4ca671724ae2f8c2a9eb3a1bf790fc3bc8d6e7ef
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-11-08 18:59:49 +0100

    create edit branche view

---
+++ resources/views/branch/edit.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+    <x-slot name="styles">
+    </x-slot>
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Branches</h1>
+                    </div>
+                    <div class="col-sm-6">
+
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <!-- Default box -->
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Edit {{ $branch->name }}</h3>
+                            </div>
+                            <form method="POST" action="{{ route('branch.update', ['branch' => $branch]) }}">
+                                @csrf
+                                @method('PATCH')
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="branch">Branch</label>
+                                        <input type="text" name="name" value="{{ old('name', $branch->name) }}"
+                                            class="form-control @error('name') is-invalid @enderror" id="branch"
+                                            placeholder="Enter branch">
+                                        @error('name')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <x-slot name="scripts"></x-slot>
+</x-app-layout>
