bf5f430ce9b70bc400ca69c3c404db5f0b70cf1b
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-11-11 21:13:08 +0100

    use branchClassroom instead of classroom

--- app/Http/Middleware/CheckUserIsStudentClassroomTeacher.php
+++ app/Http/Middleware/CheckUserIsStudentClassroomTeacher.php
@@ -19,10 +19,10 @@ class CheckUserIsStudentClassroomTeacher
         if (auth('teacher')->check()) {
 
             //if request teacher doesn't have a classroom
-            if (is_null($request->user()->classroom)) abort(403);
+            if (is_null($request->user()->branchClassroom)) abort(403);
 
             //check if authenticated teacher is the classteacher of the classroom to be viewed
-            if (!$request->user()->classroom->id === $request->route('student')->classroom->id) abort(403);
+            if (!$request->user()->branchClassroom?->id === $request->route('student')->branchClassroom?->id) abort(403);
             
         } else abort(403);
 
