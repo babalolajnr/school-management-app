917a2305ffc6a434ca9d1b733a21e65f9a456b32
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-11-13 21:48:10 +0100

    🆕 change font to nunito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,21 @@
     {{ $styles }}
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('TAssets/dist/css/adminlte.min.css') }}">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;0,700;1,200;1,400;1,700&display=swap"
+        rel="stylesheet">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <style>
+        * {
+            font-family: 'Nunito', sans-serif;
+        }
+
+    </style>
 </head>
 
 <body class="hold-transition sidebar-mini layout-navbar-fixed layout-fixed layout-footer-fixed">
