221530e98f0a7b9ee5c07884e27fcc60fcb8304d
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-02-10 16:46:04 +0100

    add auth middleware to register routes

--- routes/auth.php
+++ routes/auth.php
@@ -14,11 +14,6 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest:web,teacher')->group(function () {
-    
-    Route::get('/register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('/register', [RegisteredUserController::class, 'store']);
 
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
@@ -36,6 +31,12 @@ Route::middleware('guest:web,teacher')->group(function () {
 
     Route::post('/reset-password', [NewPasswordController::class, 'store'])
         ->name('password.update');
+});
+
+Route::controller(RegisteredUserController::class)->middleware('auth')->group(function () {
+
+    Route::get('/register', 'create');
+    Route::post('/register', 'store');
 });
 
 Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
