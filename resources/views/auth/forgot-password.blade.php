2d3957ec351210d24fc568d5614bc6970b2c691e
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-04-06 05:39:47 +0100

    redesign forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,40 @@
-<x-guest-layout>
-    <x-auth-card>
-        {{-- <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot> --}}
+<!DOCTYPE html>
+<html lang="en">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('images/radiant_logo-removebg-preview.png') }}" type="image/x-icon">
+    <title>Forgot Password</title>
+</head>
+
+<body
+    class="bg-gradient-to-r from-violet-500 to-fuchsia-900 w-screen flex h-screen sans-pro justify-center items-center">
+    <div class="bg-slate-200 md:w-1/3 rounded-lg shadow-lg mx-5 md:mx-0">
+        <div class="px-4 py-5">
+            <div class="text-sm">
+                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            </div>
+            <form action="{{ route('password.email') }}" class="mt-4">
+                <label for="Email" class="font-bold text-sm">Email</label><br />
+                <input autocomplete="email" name="email" type="email" placeholder="Enter your email"
+                    class="rounded-lg w-full px-3 bg-transparent focus:shadow-md focus:outline-none border placeholder:font-light py-1 border-slate-300"
+                    required />
+                @error('email')
+                    <div class="text-red-500 text-sm">{{ $message }}</div>
+                @enderror
+                <div class="mt-4 flex flex-row-reverse">
+                    <button type="submit"
+                        class="bg-slate-900 text-slate-200 text-sm group hover:bg-gradient-to-r hover:from-violet-500 hover:to-fuchsia-500 py-2 px-4 rounded-lg">
+                        {{ __('Send Password Reset Link') }}
+                    </button>
+            </form>
         </div>
+    </div>
+</body>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</html>
