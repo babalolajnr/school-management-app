4d1bef1fd085e6e468f96d221fd288d00b219cae
Abdulqudduus <babalolajnr@gmail.com>
2022-04-23 16:27:54 +0100

    format and add comments

--- app/Http/Controllers/AcademicSessionController.php
+++ app/Http/Controllers/AcademicSessionController.php
@@ -6,13 +6,19 @@ use App\Models\AcademicSession;
 use App\Traits\ValidationTrait;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-use Illuminate\Validation\ValidationException;
 
 class AcademicSessionController extends Controller
 {
     use ValidationTrait;
 
-    private function validateAcademicSession($request, $academicSession = null)
+    /**
+     * validate Academic Session
+     *
+     * @param  mixed $request
+     * @param  mixed $academicSession
+     * @return array
+     */
+    private function validateAcademicSession($request, $academicSession = null): array
     {
         $messages = [
             'name.required' => 'This field is required',
@@ -21,9 +27,21 @@ class AcademicSessionController extends Controller
         ];
 
         $validatedData = $request->validate([
-            'name' => ['required', 'string', Rule::unique('academic_sessions')->ignore($academicSession), 'regex:/^\d{4}[-]{1}\d{4}$/m'],
-            'start_date' => ['required', 'date', Rule::unique('academic_sessions')->ignore($academicSession)],
-            'end_date' => ['required', 'date', Rule::unique('academic_sessions')->ignore($academicSession), 'after:start_date']
+            'name' => [
+                'required', 'string',
+                Rule::unique('academic_sessions')->ignore($academicSession),
+                'regex:/^\d{4}[-]{1}\d{4}$/m'
+            ],
+            'start_date' => [
+                'required',
+                'date',
+                Rule::unique('academic_sessions')->ignore($academicSession)
+            ],
+            'end_date' => [
+                'required',
+                'date',
+                Rule::unique('academic_sessions')->ignore($academicSession), 'after:start_date'
+            ]
         ], $messages);
 
         return $validatedData;
@@ -31,10 +49,10 @@ class AcademicSessionController extends Controller
 
     public function edit(AcademicSession $academicSession)
     {
-        // Log activity
-        \activity()->causedBy(auth()->user())
-        ->on($academicSession)
-        ->log("Requested Academic Session edit form");
+        // // Log activity
+        // \activity()->causedBy(auth()->user())
+        //     ->on($academicSession)
+        //     ->log("Requested Academic Session edit form");
 
         return view('academic-session.edit', compact('academicSession'));
     }
@@ -44,7 +62,12 @@ class AcademicSessionController extends Controller
         $data = $this->validateAcademicSession($request, $academicSession);
 
         //check if date range is unique
-        $validateDateRange = $this->validateDateRange($data['start_date'], $data['end_date'], AcademicSession::class, $academicSession);
+        $validateDateRange = $this->validateDateRange(
+            $data['start_date'],
+            $data['end_date'],
+            AcademicSession::class,
+            $academicSession
+        );
 
         if ($validateDateRange !== true) {
             return back()->with('error', 'Date range overlaps with another Academic session');
@@ -52,10 +75,10 @@ class AcademicSessionController extends Controller
 
         $academicSession->update($data);
 
-        // Log activity
-        \activity()->causedBy(auth()->user())
-        ->on($academicSession)
-        ->log("Updated Academic Session");
+        // // Log activity
+        // \activity()->causedBy(auth()->user())
+        //     ->on($academicSession)
+        //     ->log("Updated Academic Session");
 
         return redirect()->route('academic-session.index')->with('success', 'Academic Session Updated!');
     }
