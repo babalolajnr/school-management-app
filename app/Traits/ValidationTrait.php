a68f4d939707fb5391989f8d4efcc410146ae778
Abdulqudduus <babalolajnr@gmail.com>
2022-04-23 19:45:16 +0100

    rename validateDateRange to dateOverlaps and refactor

--- app/Traits/ValidationTrait.php
+++ app/Traits/ValidationTrait.php
@@ -8,14 +8,14 @@ trait ValidationTrait
 {
 
     /**
-     * Validate date range is unique and does not
+     * Validate input date overlaps does not
      * overlap other date ranges
-     * 
+     *
      * For Two Date ranges A and B
      * Formula : StartA <= EndB && EndA >= StartB
-     * 
-     * If the function returns true, date range is unique
-     * else it is not
+     *
+     * If the function returns true, input date overlaps
+     * else it does not
      *
      * @param  string $startDate
      * @param  string $endDate
@@ -23,35 +23,26 @@ trait ValidationTrait
      * @param object $ignore
      * @return bool
      */
-    private function validateDateRange($startDate, $endDate, $model, $ignore = null): bool
+    private function dateOverlaps(string $startDate, string $endDate, string $model, object $ignore = null): bool
     {
         $records = $model::all();
 
-        if (count($records) < 1) {
-            return true;
-        }
+        if (count($records) < 1) return false;
 
         $startDate = Carbon::createFromFormat('Y-m-d', $startDate);
         $endDate = Carbon::createFromFormat('Y-m-d', $endDate);
 
         foreach ($records as $record) {
 
+            //ignore given record
+            if ($record->id == $ignore?->id) continue;
+
             $recordStartDate =  Carbon::createFromFormat('Y-m-d', $record->start_date);
             $recordEndDate =  Carbon::createFromFormat('Y-m-d', $record->end_date);
 
-            if ($recordStartDate->lessThanOrEqualTo($endDate) && $recordEndDate->greaterThanOrEqualTo($startDate)) {
-
-                //ignore given record by returning true if it overlaps itself
-                if ($ignore != null) {
-                    if ($record->id == $ignore->id) {
-                        continue;
-                    }
-                }
-
-                return false;
-            }
+            if ($recordStartDate->lessThanOrEqualTo($endDate) && $recordEndDate->greaterThanOrEqualTo($startDate)) return true;
         }
-
-        return true;
+        
+        return false;
     }
 }
