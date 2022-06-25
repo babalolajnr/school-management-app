ace2c6dc68704aafe304b6290962513080e9c24c
Abdulqudduus <babalolajnr@gmail.com>
2022-06-25 10:28:40 +0100

    feat: make guardian model authenticable

--- app/Models/Guardian.php
+++ app/Models/Guardian.php
@@ -2,20 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Passwords\CanResetPassword as PasswordsCanResetPassword;
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Guardian extends Model
+class Guardian extends Authenticatable implements CanResetPassword
 {
-    use HasFactory;
-    
+    use HasFactory, Notifiable, PasswordsCanResetPassword;
+
     /**
      * The attributes that are not mass assignable
      *
      * @var array
      */
     protected $guarded = ['id', 'created_at', 'updated_at'];
-    
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     /**
      * Student relationship.
      *
