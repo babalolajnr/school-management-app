b6a5bf42f7231ba37e7748a3c895ed1268f852b9
Abdulqudduus Babalola <babalolajnr@gmail.com>
2023-04-07 19:35:15 +0100

    Fix: add correct info

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -12,50 +12,41 @@
     <title>Reset Password</title>
 </head>
 
-<body
-    class="bg-gradient-to-r from-violet-500 to-fuchsia-900 w-screen flex h-screen sans-pro justify-center items-center">
+<body class="bg-gradient-to-r from-violet-500 to-fuchsia-900 w-screen flex h-screen sans-pro justify-center items-center">
     <div class="bg-slate-200 md:w-1/3 rounded-lg shadow-lg mx-5 md:mx-0">
         <div class="px-4 py-5">
             <div class="text-sm">
-                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                {{ __('Reset your password here') }}
             </div>
             <form action="{{ route('password.update') }}" method="POST" class="mt-4">
                 @csrf
                 <!-- Password Reset Token -->
                 <input type="hidden" name="token" value="{{ $request->route('token') }}">
-                
+
                 <div>
                     <label for="Email" class="font-bold text-sm">Email</label><br />
-                    <input autocomplete="email" autofocus name="email" value="{{ old('email', $request->email) }}"
-                        type="email" placeholder="Enter your email"
-                        class="rounded-lg w-full px-3 bg-transparent focus:shadow-md focus:outline-none border placeholder:font-light py-1 border-slate-300"
-                        required />
+                    <input autocomplete="email" autofocus name="email" value="{{ old('email', $request->email) }}" type="email" placeholder="Enter your email" class="rounded-lg w-full px-3 bg-transparent focus:shadow-md focus:outline-none border placeholder:font-light py-1 border-slate-300" required />
                     @error('email')
-                        <div class="text-red-500 text-sm">{{ $message }}</div>
+                    <div class="text-red-500 text-sm">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mt-2">
                     <label for="password" class="font-bold text-sm">Password</label><br />
-                    <input name="password" type="password" placeholder="Enter a new password"
-                        class="rounded-lg w-full px-3 bg-transparent focus:shadow-md focus:outline-none border placeholder:font-light py-1 border-slate-300"
-                        required />
+                    <input name="password" type="password" placeholder="Enter a new password" class="rounded-lg w-full px-3 bg-transparent focus:shadow-md focus:outline-none border placeholder:font-light py-1 border-slate-300" required />
                     @error('password')
-                        <div class="text-red-500 text-sm">{{ $message }}</div>
+                    <div class="text-red-500 text-sm">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mt-2">
                     <label for="password_confirmation" class="font-bold text-sm">Confirm Password</label><br />
-                    <input name="password_confirmation" type="password" placeholder="Confirm Password"
-                        class="rounded-lg w-full px-3 bg-transparent focus:shadow-md focus:outline-none border placeholder:font-light py-1 border-slate-300"
-                        required />
+                    <input name="password_confirmation" type="password" placeholder="Confirm Password" class="rounded-lg w-full px-3 bg-transparent focus:shadow-md focus:outline-none border placeholder:font-light py-1 border-slate-300" required />
                     @error('password_confirmation')
-                        <div class="text-red-500 text-sm">{{ $message }}</div>
+                    <div class="text-red-500 text-sm">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mt-4 flex flex-row-reverse">
-                    <button type="submit"
-                        class="bg-slate-900 text-slate-200 text-sm group hover:bg-gradient-to-r hover:from-violet-500 hover:to-fuchsia-500 py-2 px-4 rounded-lg">
-                        {{ __('Send Password Reset Link') }}
+                    <button type="submit" class="bg-slate-900 text-slate-200 text-sm group hover:bg-gradient-to-r hover:from-violet-500 hover:to-fuchsia-500 py-2 px-4 rounded-lg">
+                        {{ __('Reset Password') }}
                     </button>
             </form>
         </div>
